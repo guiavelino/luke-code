f233327996c088e2ae4d66706bc3dc87cdd2f9b0
Guilherme Avelino <guilhermeavelino1@outlook.com>
2020-12-10 18:43:46 -0300

    Starting project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Home from './pages/Home';
+
+import './assets/styles/global.css';
+
+const App = () => {
+    return (
+        <>
+            <Home />
+        </>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './styles.css';
+
+import yoda from '../../assets/images/characters/yoda.png';
+import separator from '../../assets/images/separator.svg';
+
+const Home = () => {
+    return (
+        <>
+            <div className="section-1">
+                <div className="container">
+                    <header className="header">
+                        <h2>Luke Code</h2>
+                        <nav>
+                            <button>
+                                FAZER LOGIN
+                            </button>
+                            <button>
+                                INSCREVA-SE
+                            </button>
+                        </nav>
+                    </header>
+                    <main>
+                        <section>
+                            <h1>Se torne um mestre <br/> da programação!</h1>
+                            <img src={separator} alt="" />
+                            <p>
+                                De o próximo passo para o seu futuro, <br />
+                                que a força esteja com você.
+                            </p>
+                        </section>
+                        <section>
+                            <img src={yoda} alt="Yoda - Star Wars" />
+                        </section>
+                    </main>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
